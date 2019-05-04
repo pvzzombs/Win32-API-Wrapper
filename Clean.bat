e4564c03b4a5ac47eea1bfc82f0a66c3b43e72d1
Ian Vincent Slesser <ivslesser@Belindas-iMac.lan>
2019-05-04 08:40:24 +0100

    Clean Up

--- Clean.bat
+++ Clean.bat
@@ -2,5 +2,6 @@ rmdir /S/Q bin
 rmdir /S/Q bin-int
 rmdir /S/Q node_modules
 rmdir /S/Q .vscode
+rmdir /S/Q .vs
 del /F/Q/S *.vcx*
 del /F/Q/S *.sln
---
+++ Clean.sh
@@ -0,0 +1,10 @@
+# Manually clear up files
+rm -rf bin
+rm -rf bin-int
+rm -rf node_modules
+rm -rf .vscode
+rm -rf .vs
+rm -rf **/*.xc*
+rm -rf *.xcworkspace
+rm -rf **/*.vc*
+rm -rf *.sln
